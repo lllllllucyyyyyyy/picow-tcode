706671995c410087e241e8256be1aec7fed5c243
lllllllucyyyyyyy <lllllllucyyyyyyy@gmail.com>
2025-04-04 06:09:03 -0500

    that was easier than expected

--- log.md
+++ log.md
@@ -6,4 +6,8 @@ Turns out the I was editing the wrong file, I was editing the user file instead 
 
 ## even later
 I was wondering why I couldn't get the program to detect writes... turns out it's the dynamic gatt trait? (idk what they're called, but its in the same spot as where youd put write and notify and so on) that lets the program actually use that data. That was a fun half hour of debugging. 
-//TODO: get a utf-8 string out of whatever garbled nonsense the bluetooth writes send, and spew it into the console. baby steps :)
+//TODO (done): get a utf-8 string out of whatever garbled nonsense the bluetooth writes send, and spew it into the console. baby steps :)
+
+# 4/4/25
+Turns out literally printing the incoming buffer let me see the strings coming in. Copying that should let me use the contents.
+//TODO: separate bluetooth commands into their own file, start decoding incoming t code.
--- main.c
+++ main.c
@@ -25,6 +25,9 @@ static int val = 5;
 static char val_str[30];
 static int val_len;
 
+void* string_get_callback;
+//Zack Freedman reference?
+
 #define APP_AD_FLAGS 0x06
 
 
@@ -120,6 +123,9 @@ static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_h
         case ATT_CHARACTERISTIC_F0DAC9F2_06B0_4725_A80A_FF083A09A857_01_VALUE_HANDLE:
             printf("Write: transaction mode %u, offset %u, data (%u bytes): ", transaction_mode, offset, buffer_size);
             printf_hexdump(buffer, buffer_size);
+            printf(buffer);
+            uint8_t* data;
+            data = buffer;
             break;
         default:
             break;
@@ -170,6 +176,8 @@ int main()
 	    
     // Run our Bluetooth app
     printf("running");
-    btstack_run_loop_execute();
-    //while(1) ;
+    //btstack_run_loop_execute();
+    while(true) {      
+        sleep_ms(1000);
+    }
 }
