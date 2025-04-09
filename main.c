a67ac75eb383486ea7426f0cd8a8011b3cf54816
lllllllucyyyyyyy <lllllllucyyyyyyy@gmail.com>
2025-04-08 21:30:06 -0500

    made into an actual fork, but maybe I should do this differently...

--- main.c
+++ main.c
@@ -5,45 +5,49 @@
 #include "bluetooth_manager.h"
 #include "tcode_processor.h"
 
-#include "hardware/pwm.h"
+#include "servo.h"
+
+bool direction = true;
+int pos = 1500;
+int servoPin0 = 2;
+int servoPin1 = 3;
+
+int old_timestamp;
+int target_time;
+int old_pos;
+int target_pos;
+bool movecommand = false;
+
+int zero = 1250;
+int range = 500;
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 
 void route_data(uint8_t *buffer, uint8_t length)
 {
-    if (command.axis == VIBRATION)
-    {
-        if (command.magnitude == 0)
+    struct tcode_command_t command = process_tcode(buffer, length);
+    if (command.axis == 0)
         {
-            switch (command.channel)
+            if (command.channel == 0)
             {
-            case 0:
-                pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);
-                break;
-            case 1:
-                pwm_set_chan_level(slice_num, PWM_CHAN_B, 0);
-                break;
+                old_pos = pos;
+                target_pos = (command.magnitude * range) + zero;
+                old_timestamp = (get_absolute_time() / 1000);
+                target_time = command.time;
+                movecommand = true;
             }
         }
-        else
-        {
-            int vel = (command.magnitude * 60) + 40;
-            switch (command.channel)
-            {
-            case 0:
-                pwm_set_chan_level(slice_num, PWM_CHAN_A, vel;
-                break;
-            case 1:
-                pwm_set_chan_level(slice_num, PWM_CHAN_B, vel);
-                break;
-            }
-        }
-    }
 }
 
 int main()
 {
     stdio_init_all();
 
-    if (cyw43_arch_init())
+    /*if (cyw43_arch_init())
     {
         printf("failed to initialise cyw43_arch\n");
         return -1;
@@ -51,21 +55,12 @@ int main()
     else
     {
         printf("initialization successful");
-    }
+    }*/
 
-    gpio_set_function(2, GPIO_FUNC_PWM);
-    gpio_set_function(3, GPIO_FUNC_PWM);
-    pwm_config config = pwm_get_default_config();
-
-    uint slice_num = pwm_gpio_to_slice_num(2);
-    pwm_set_wrap(slice_num, 100);
-    pwm_set_enabled(slice_num, true);
-
-    ble_init();
+   //ble_init();
+   //bt_string_get_callback = *route_data;
 
     printf("running");
-
-    bt_string_get_callback = *route_data;
 
     static char message[15];
     static unsigned int message_pos = 0;
